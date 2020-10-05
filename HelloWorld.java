fb0c09cd8bc7fd34d51af6128e5b8d18eb226210
username <username@gmail.com>
2020-10-05 21:05:27 +0800

    34

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class HelloWorld{
    public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+
 
     }
 
